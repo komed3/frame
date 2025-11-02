d2597b6168cb1df09bb2f2f5dc65232e2bfad6d1
komed3 <pk97@outlook.de>
2025-11-02 12:36:28 +0100

    init controls

--- public/js/player.js
+++ public/js/player.js
@@ -5,6 +5,20 @@ class VideoPlayer {
         this.container = document.getElementById( 'player' );
         this.video = this.container.querySelector( 'video' );
 
+        this.controls = this.#initControls();
+
+    }
+
+    #initControls () {
+
+        const controls = {};
+
+        this.container.querySelectorAll( '.player-controls [action]' ).forEach( el => {
+            controls[ el.getAttribute( 'action' ) ] = el;
+        } );
+
+        return controls;
+
     }
 
 }
