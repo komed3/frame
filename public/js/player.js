d77676579992d45cddb362b4c1b67b3fe69feff2
komed3 <pk97@outlook.de>
2025-11-02 14:36:08 +0100

    init key bindings

--- public/js/player.js
+++ public/js/player.js
@@ -1,5 +1,9 @@
 class VideoPlayer {
 
+    bindings = [
+        ' ', 'k', 'K'
+    ];
+
     constructor () {
 
         this.container = document.getElementById( 'player' );
@@ -7,6 +11,7 @@ class VideoPlayer {
         this.controls = this.initControls();
 
         this.initEventHandlers();
+        this.initKeyBindings();
 
     }
 
@@ -29,6 +34,28 @@ class VideoPlayer {
 
     }
 
+    initKeyBindings () {
+
+        document.addEventListener( 'keydown', ( e ) => {
+
+            if ( e.target.tagName === 'input' || ! this.bindings.includes( e.key ) ) return;
+
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            e.stopPropagation();
+
+            switch ( e.key ) {
+
+                case ' ': case 'k': case 'K':
+                    this.togglePlay();
+                    break;
+
+            }
+
+        } );
+
+    }
+
     async play () {
 
         await this.video.play();
@@ -44,6 +71,13 @@ class VideoPlayer {
 
         this.controls.play.disabled = false;
         this.controls.pause.disabled = true;
+
+    }
+
+    async togglePlay () {
+
+        if ( this.video.paused ) await this.play();
+        else this.pause();
 
     }
 
