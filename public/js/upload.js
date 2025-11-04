eea67217afc35c6c18f3d769352b5af95e12e409
komed3 <pk97@outlook.de>
2025-11-04 23:02:37 +0100

    add event handlers

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,6 +1,64 @@
 class VideoUploader {
 
-    constructor () {}
+    constructor () {
+
+        this.container = document.querySelector( '.upload-container' );
+        this.dropArea = this.container.querySelector( '.file-drop-area' );
+        this.fileInput = this.container.querySelector( '[name="video"]' );
+
+        this.initEventHandlers();
+
+    }
+
+    initEventHandlers () {
+
+        // Drag and drop events
+        [ 'dragenter', 'dragover', 'dragleave', 'drop' ].forEach( event => {
+
+            this.dropArea.addEventListener( event, e =>
+                e.preventDefault() && e.stopPropagation()
+            );
+
+        } );
+
+        [ 'dragenter', 'dragover' ].forEach( event => {
+
+            this.dropArea.addEventListener( event, () => {
+                this.dropArea.classList.add( 'dragover' );
+            } );
+
+        } );
+
+        [ 'dragleave', 'drop' ].forEach( event => {
+
+            this.dropArea.addEventListener( event, () => {
+                this.dropArea.classList.remove( 'dragover' );
+            } );
+
+        } );
+
+        // Handle file selection
+        this.dropArea.addEventListener( 'drop', e => {
+
+            const file = e.dataTransfer.files[ 0 ];
+
+            if ( file && file.type.startsWith( 'video/' ) ) {
+                this.handleFileSelect( file );
+            }
+
+        } );
+
+        this.fileInput.addEventListener( 'change', e => {
+
+            const file = e.target.files[ 0 ];
+
+            if ( file ) this.handleFileSelect( file );
+
+        } );
+
+    }
+
+    handleFileSelect ( file ) {}
 
 }
 
