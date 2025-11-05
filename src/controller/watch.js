1c97491c5e2e0d02cf5fac9718af335dd5ce7037
komed3 <pk97@outlook.de>
2025-11-05 23:43:46 +0100

    get video data from id

--- src/controller/watch.js
+++ src/controller/watch.js
@@ -1,5 +1,16 @@
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+
 export function watch ( req, res ) {
 
-    res.render( 'watch', {} );
+    if ( ! req.params || ! req.params.id ) res.redirect( '/' );
+
+    const videoId = req.params.id;
+    const videoDir = join( process.cwd(), 'media', videoId );
+    const data = JSON.parse( readFileSync( join( videoDir, 'data.json' ), 'utf8' ) || '{}' );
+    const stream = '/media/' + videoId + '/' + data.fileName;
+
+    if ( ! data || ! data.videoId || ! data.fileId ) return res.redirect( '/' );
+    else res.render( 'watch', { video: data, stream } );
 
 }
