bcaa0c10bf0f3cc660cb86b5901fcc383872325f
komed3 <pk97@outlook.de>
2025-11-05 10:03:49 +0100

    prepare for file upload

--- src/api.js
+++ src/api.js
@@ -1,4 +1,16 @@
+import { analyseVideo } from './utils/analyseVideo.js';
+import { mkdir, writeFile, rename } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
 import express from 'express';
+import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import ShortUniqueId from 'short-unique-id';
+
+// Init ID generator
+new uid = new ShortUniqueId( {
+    dictionary: 'alphanum',
+    length: 10
+} );
 
 // Init express router
 const api = express.Router();
---
+++ src/utils/analyseVideo.js
@@ -0,0 +1,1 @@
+export function analyseVideo ( file ) {}
