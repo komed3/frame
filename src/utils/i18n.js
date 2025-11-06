a595dfa93aa496c593e9a9ceec3641360057240b
komed3 <pk97@outlook.de>
2025-11-06 17:28:38 +0100

    add language detection

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,17 +1,24 @@
 import { join } from 'node:path';
 import i18next from 'i18next';
 import FsBackend from 'i18next-fs-backend';
-import { handle } from 'i18next-http-middleware';
+import { LanguageDetector, handle } from 'i18next-http-middleware';
 import { cwd } from './config.js';
 
-await i18next.use( FsBackend ).init( {
+await i18next.use( FsBackend ).use( LanguageDetector ).init( {
     fallbackLng: 'en-US',
     preload: [ 'en-US', 'de-DE' ],
+    cleanCode: true,
     backend: {
         loadPath: join( cwd, 'locales/{{lng}}.json' )
     },
     interpolation: {
         escapeValue: false
+    },
+    detection: {
+        order: [ 'cookie', 'header' ],
+        lookupCookie: 'locale',
+        caches: [ 'cookie' ],
+        cookieSameSite: 'strict'
     }
 } );
 
