e559385848069f9b293d2ade37d0247274cd55cb
komed3 <pk97@outlook.de>
2025-11-06 16:05:14 +0100

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import { join } from 'node:path';
+import express, { static as serveStatic } from 'express';
+import { cwd } from './utils/config.js';
+
+// Initialize express app
+const app = express();
+
+// Set view engine
+app.set( 'views', join( cwd, 'views' ) );
+app.set( 'view engine', 'pug' );
+
+// Middlewares
+app.use( express.urlencoded( { extended: true } ) );
+app.use( express.json() );
+
+// Serve static files
+app.use( '/fonts', serveStatic( join( cwd, 'public/fonts' ) ) );
+app.use( '/images', serveStatic( join( cwd, 'public/images' ) ) );
+app.use( '/css', serveStatic( join( cwd, 'public/css' ) ) );
+app.use( '/js', serveStatic( join( cwd, 'public/js' ) ) );
+app.use( '/media', serveStatic( join( cwd, 'media' ) ) );
+
+// App listen on port
+app.listen( process.env.PORT || 3000, () => {
+    console.log( 'Server is running!' );
+} );
