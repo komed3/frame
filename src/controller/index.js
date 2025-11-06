079f830d7b9dac3e7da48dce5d48902bd18b33c0
komed3 <pk97@outlook.de>
2025-11-06 23:53:07 +0100

    simplify routing

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,25 +1,12 @@
 import express from 'express';
-import { home } from './home.js';
 
 // Init express router
 const router = express.Router();
 
-// Setup routes
-const routes = [
-    { path: '{/}', get: home }
-];
-
-// Routing paths
-routes.forEach( ( route ) => {
-
-    const { path, get, post } = route;
-
-    if ( post ) router.post( path, post );
-    if ( get ) router.get( path, get );
-
-} );
+// Handle routes
+router.get( '{/}', ( req, res ) => res.render( 'home', { title: req.t( 'views.home.title' ) } ) );
 
 // Handle unknown paths
 router.get( '/{*splat}', ( _, res ) => res.redirect( '/' ) );
 
-export { routes, router };
+export { router };
