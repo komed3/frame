276d8bb746eb89e10e7dcc5cb95ae2827b41a8e6
komed3 <pk97@outlook.de>
2025-11-08 12:56:49 +0100

    upload video file

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,1 +1,14 @@
-export async function upload ( req, res ) {}
+import {} from '../utils/config.js';
+import {} from '../utils/search.js';
+import { uploadVideo } from '../utils/video.js';
+
+export async function upload ( req, res ) {
+
+    uploadVideo( req, res, async ( err ) => {
+
+        if ( err ) return res.status( 400 ).json( { success: false, message: req.t( 'error.upload.upload', { msg: err.message } ) } );
+        if ( ! req.file ) return res.status( 400 ).json( { success: false, message: req.t( 'error.upload.noFile' ) } );
+
+    } );
+
+}
