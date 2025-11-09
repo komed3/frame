1f7c87eec76ffa417d55de0b749ddbfbad16d206
komed3 <pk97@outlook.de>
2025-11-09 02:17:00 +0100

    Update global.js

--- public/js/global.js
+++ public/js/global.js
@@ -1,3 +1,6 @@
+const lang = document.documentElement.getAttribute( 'lang' );
+const template = document.documentElement.getAttribute( 'template' );
+
 const formatTime = ( seconds, includeHrs = false ) => {
 
     if ( isNaN( seconds ) ) return '00:00';
@@ -18,8 +21,32 @@ const formatFileSize = ( bytes, digits = 1 ) => {
 
     while ( bytes >= 1024 && i < units.length - 1 ) bytes /= 1024, i++;
 
-    return `${ new Intl.NumberFormat( 'en-US', {
+    return `${ new Intl.NumberFormat( lang, {
         maximumFractionDigits: digits
     } ).format( bytes ) } ${ units[ i ] }`;
 
 };
+
+const formatViews = ( views ) => {
+
+    return new Intl.NumberFormat( 'en-US', {
+        notation: 'compact', compactDisplay: 'short'
+    } ).format( views );
+
+}
+
+document.addEventListener( 'DOMContentLoaded', function () {
+
+    document.querySelectorAll( 'time' ).forEach(
+        el => el.textContent = formatTime( el.textContent )
+    );
+
+    document.querySelectorAll( 'filesize' ).forEach(
+        el => el.textContent = formatFileSize( el.textContent )
+    );
+
+    document.querySelectorAll( 'views' ).forEach(
+        el => el.textContent = formatViews( el.textContent )
+    );
+
+} );
