1f5775f3e951ee3ec7e860273a37a111fff79145
komed3 <pk97@outlook.de>
2025-11-09 09:21:26 +0100

    get video (meta) data

--- src/api/video.js
+++ src/api/video.js
@@ -1,5 +1,14 @@
+import { access, readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { media } from '../utils/config.js';
+
 export async function video ( req, res ) {
 
-    //
+    const videoId = req.params.id || '';
+    const videoDir = join( media, videoId );
+
+    if ( ! await access( videoDir ) ) res.status( 400 ).json( { msg: req.t( 'error.video.notExists' ) } );
+    else try { res.status( 200 ).json( JSON.parse( await readFile( join( videoDir, 'video.json' ) ) ) ) }
+    catch ( err ) { res.status( 500 ).json( { msg: req.t( 'error.video.data' ) } ) }
 
 }
