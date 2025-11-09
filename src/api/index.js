97fb0d6f4925f669fda4d2ce2fe0a49aea903ff3
komed3 <pk97@outlook.de>
2025-11-09 09:09:16 +0100

    Create api/video.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { upload } from './upload.js';
+import { video } from './video.js';
 
 // Init express api
 const api = express.Router();
 
 // Handle routes
+api.get( '/api/video/:id', video );
 api.post( '/api/upload', upload );
 
 export { api };
