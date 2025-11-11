850f7b167febdb2a3b17d8f5bbb44fa9d96f012f
komed3 <pk97@outlook.de>
2025-11-11 11:06:46 +0100

    remove not working stream api

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import { upload } from './upload.js';
-import { stream, video } from './video.js';
+import { video } from './video.js';
 
 // Init express api
 const api = express.Router();
 
 // Handle routes
 api.post( '/api/video/:id', video );
-api.post( '/api/stream', stream );
 api.post( '/api/upload', upload );
 
 export { api };
