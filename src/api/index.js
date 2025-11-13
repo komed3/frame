677c7d6f76bda0c5601e8dfba9590963f6a6190d
komed3 <pk97@outlook.de>
2025-11-13 23:48:15 +0100

    fix API routing

--- src/api/index.js
+++ src/api/index.js
@@ -7,13 +7,13 @@ import { video } from './video.js';
 const api = express.Router();
 
 // Handle routes
+api.post( '/api/video/:id', video );
+api.post( '/api/upload', upload );
+api.post( '/api/list/new', createList );
 api.post( '/api/list/:id', getPlaylist );
-api.post( '/api/list/:id/rename', renameList );
-api.post( '/api/list/:id/delete', deleteList );
 api.post( '/api/list/:id/add', addToList );
 api.post( '/api/list/:id/rmv', removeFromList );
-api.post( '/api/list/new', createList );
-api.post( '/api/upload', upload );
-api.post( '/api/video/:id', video );
+api.post( '/api/list/:id/rename', renameList );
+api.post( '/api/list/:id/delete', deleteList );
 
 export { api };
