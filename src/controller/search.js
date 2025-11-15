06b9be11f9bf211e065716ddc272d5920d4331a4
komed3 <pk97@outlook.de>
2025-11-15 09:22:48 +0100

    redirect empty query

--- src/controller/search.js
+++ src/controller/search.js
@@ -3,23 +3,28 @@ import { searchIndex } from '../utils/search.js';
 export async function search ( req, res ) {
 
     const query = req.query.q || '';
-    const sort = req.query.sort || 'relevance';
-    const order = req.query.order || 'desc';
-    const offset = parseInt( req.query.offset ) || 0;
-    const limit = parseInt( req.query.limit ) || 24;
 
-    const filters = {};
+    if ( ! query ) { res.redirect( '/' ) } else {
 
-    const authors = await searchIndex.getAuthors();
-    const categories = await searchIndex.getCategories();
-    const tags = await searchIndex.getTags();
-    const years = await searchIndex.getYears();
+        const sort = req.query.sort || 'relevance';
+        const order = req.query.order || 'desc';
+        const offset = parseInt( req.query.offset ) || 0;
+        const limit = parseInt( req.query.limit ) || 24;
 
-    res.render( 'search', {
-        title: req.t( 'views.search.title' ),
-        path: '/search', template: 'search',
-        query, filters, sort, order,
-        authors, categories, tags, years
-    } );
+        const filters = {};
+
+        const authors = await searchIndex.getAuthors();
+        const categories = await searchIndex.getCategories();
+        const tags = await searchIndex.getTags();
+        const years = await searchIndex.getYears();
+
+        res.render( 'search', {
+            title: req.t( 'views.search.title' ),
+            path: '/search', template: 'search',
+            query, filters, sort, order,
+            authors, categories, tags, years
+        } );
+
+    }
 
 }
