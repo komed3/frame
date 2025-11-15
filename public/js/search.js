3d5edf9e52ac7c373ed1e43d379e7d623555c2d3
komed3 <pk97@outlook.de>
2025-11-15 18:28:06 +0100

    Update search.js

--- public/js/search.js
+++ public/js/search.js
@@ -4,10 +4,21 @@ class VideoSearch {
 
         this.form = document.querySelector( '.frame-search--form' );
         this.empty = document.querySelector( '.frame-search--empty' );
+        this.loader = document.querySelector( '.frame-search--loader' );
         this.results = document.querySelector( '.frame-search--results-grid' );
         this.more = document.querySelector( '.frame-search--more' );
 
+        this.initEventHandlers();
+
     }
+
+    initEventHandlers () {
+
+        this.form.addEventListener( 'submit', this.handleSubmit.bind( this ) );
+
+    }
+
+    async handleSubmit () {}
 
 }
 
