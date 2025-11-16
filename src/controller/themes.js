4b021f2a11c4eed517f704ae6aea96dda43fac21
komed3 <pk97@outlook.de>
2025-11-16 09:54:58 +0100

    fetch videos by themes

--- src/controller/themes.js
+++ src/controller/themes.js
@@ -1,8 +1,25 @@
+import { searchIndex } from '../utils/search.js';
+
 export async function themes ( req, res ) {
+
+    const categories = await searchIndex.getCategories();
+    const videos = {};
+
+    for ( const c of categories ) {
+
+        const ids = await searchIndex.findByCategory( c );
+        videos[ c ] = [];
+
+        for( const v of ids.slice( 0, 12 ) ) {
+            videos[ c ].push( await searchIndex.getVideo( v ) );
+        }
+
+    }
 
     res.render( 'themes', {
         title: req.t( 'views.themes.title' ),
-        path: '/themes', template: 'themes'
+        path: '/themes', template: 'themes',
+        categories, videos
     } );
 
 }
