deb2a536f2d34b105ba59e38f9ce474908d36c32
komed3 <pk97@outlook.de>
2025-11-17 19:13:51 +0100

    reworked playlist grid

--- src/controller/home.js
+++ src/controller/home.js
@@ -4,7 +4,7 @@ import { searchIndex } from '../utils/search.js';
 export async function home ( req, res ) {
 
     const history = await searchIndex.getVideos( await searchIndex.getHistory( 6, true ) );
-    const lists = ( await playlist.listIndex() ).slice( 0, 3 );
+    const lists = ( await playlist.listIndex() ).slice( 0, 6 );
     const { results: newest } = await searchIndex.search( '', { limit: 6 } );
 
     res.render( 'home', {
