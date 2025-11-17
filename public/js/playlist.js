910fec7f29992ea8d620e501b8f8d6e9841b4333
komed3 <pk97@outlook.de>
2025-11-17 19:43:50 +0100

    add playlist actions (delete/rename)

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -5,10 +5,28 @@ const deleteList = async ( listId ) => {
 
 }
 
+const renameList = async ( listId ) => {
+
+    await fetch( `/api/list/${listId}/rename`, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( {
+            name: prompt( 'Playlist name:', 'Untitled' )
+        } )
+    } );
+
+    location.reload();
+
+}
+
 document.addEventListener( 'DOMContentLoaded', function () {
 
     document.querySelectorAll( '[list][action="delete"]' ).forEach( ( el ) =>
         el.addEventListener( 'click', () => deleteList( el.getAttribute( 'list' ) ) )
+    );
+
+    document.querySelectorAll( '[list][action="rename"]' ).forEach( ( el ) =>
+        el.addEventListener( 'click', () => renameList( el.getAttribute( 'list' ) ) )
     );
 
 } );
