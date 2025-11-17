e7cfa708339cc0d719601924e5cc071050e21b39
komed3 <pk97@outlook.de>
2025-11-17 09:46:51 +0100

    rate the video by clicking stars

--- public/js/watch.js
+++ public/js/watch.js
@@ -1,39 +1,32 @@
-const setRating = ( videoId, rating ) => {
+const rateVideo = async ( videoId, rate ) => {
 
-    document.querySelectorAll( `.rating[id="${videoId}"]` ).forEach( ( el ) =>
-        el.textContent = rating.toFixed( 1 )
-    );
+    const res = await fetch( '/api/rate/' + videoId, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( { rating: rate } )
+    } );
 
-};
-
-const likeVideo = async ( videoId ) => {
-
-    const res = await fetch( '/api/like/' + videoId, { method: 'post' } );
     if ( ! res.ok ) return;
-
     const { rating } = await res.json();
-    if ( ! isNaN( rating ) ) setRating( videoId, rating );
 
-};
+    for ( let i = 1; i <= 5; i++ ) {
 
-const dislikeVideo = async ( videoId ) => {
+        const star = document.querySelector( `[v="${ videoId }"][r="${ i }"]` );
+        star.className = 'icon ';
 
-    const res = await fetch( '/api/dislike/' + videoId, { method: 'post' } );
-    if ( ! res.ok ) return;
+        if ( i <= rating ) star.classList.add( 'icon-star-full' );
+        else star.classList.add( 'icon-star' );
 
-    const { rating } = await res.json();
-    if ( ! isNaN( rating ) ) setRating( videoId, rating );
+    }
 
 };
 
 document.addEventListener( 'DOMContentLoaded', function () {
 
-    document.querySelectorAll( '[action="like"]' ).forEach( ( el ) =>
-        el.addEventListener( 'click', () => likeVideo( el.getAttribute( 'id' ) ) )
-    );
-
-    document.querySelectorAll( '[action="dislike"]' ).forEach( ( el ) =>
-        el.addEventListener( 'click', () => dislikeVideo( el.getAttribute( 'id' ) ) )
+    document.querySelectorAll( '[v][r]' ).forEach( ( el ) =>
+        el.addEventListener( 'click', async () => await rateVideo(
+            el.getAttribute( 'v' ), el.getAttribute( 'r' )
+        ) )
     );
 
 } );
