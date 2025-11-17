03c00303cde4233cce7d49aeedc7f467fcc796cc
komed3 <pk97@outlook.de>
2025-11-18 00:43:50 +0100

    change language

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,12 @@ import { rate, video } from './video.js';
 // Init express api
 const api = express.Router();
 
+// Language selector
+api.get( '/api/lang/:lang', ( req, res ) => {
+    req.i18n.changeLanguage( req.params.lang );
+    res.redirect( req.get( 'Referrer' ) );
+} );
+
 // Handle routes
 api.post( '/api/video/:id', video );
 api.post( '/api/list/new', createList );
