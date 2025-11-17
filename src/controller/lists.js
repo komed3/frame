687a174019432c5294c6cf02f6d9c9654d48fdf0
komed3 <pk97@outlook.de>
2025-11-17 15:51:18 +0100

    get list data

--- src/controller/lists.js
+++ src/controller/lists.js
@@ -1,5 +1,19 @@
+import { playlist } from '../utils/list.js';
+
 export async function list ( req, res ) {
 
-    res.render( 'list', {} );
+    const listId = req.params.id || '';
+    const list = await playlist.getList( listId, true );
+
+    if ( ! list ) { res.redirect( '/lists' ) } else {
+
+        res.render( 'list', {
+            title: list.name,
+            path: '/list/' + listId,
+            template: 'list',
+            listId, list
+        } );
+
+    }
 
 }
